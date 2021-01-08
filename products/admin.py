5af03426040c1dff7436dce0f8d1fbf8ca7b2f80
Abelyem <abel.yemane1@hotmail.co.uk>
2021-01-08 14:15:59 +0000

    Created products directory, added category and product class to models.py + registered these to admin.py to be able to add products and category via the admin login

---
+++ products/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'name',
+        'description',
+        'price',
+        'image',
+    )
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
