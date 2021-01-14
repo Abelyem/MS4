22cce76ea6df8004298f0d44d81aadd546a571d0
Abelyem <abel.yemane1@hotmail.co.uk>
2021-01-14 19:59:57 +0000

    removed friendly_name

--- products/models.py
+++ products/models.py
@@ -7,13 +7,9 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
-
-    def get_friendly_name(self):
-        return self.friendly_name
 
 
 class Product(models.Model):
