826565cf049fc9b68090cfbfa782b348fa687a4a
Abelyem <abel.yemane1@hotmail.co.uk>
2021-01-29 19:30:30 +0000

    Removed the exposed secret key and added this on heroku as a variable / set debug true in development

--- e_commerce_site/settings.py
+++ e_commerce_site/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jfd*)-0tz4nu=%6r^5h_emdl@r*$s7zqr_ny78i$r=r-3m&duh'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['masked-ms4.herokuapp.com', 'localhost']
 
